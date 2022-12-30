18e8f027a37ecfc19d68fb0bc67085a639933c79
yuto_ohnuki <yuto.personal92@gmail.com>
2022-12-30 04:54:22 +0000

    add data loads

--- src_py/main.py
+++ src_py/main.py
@@ -1,1 +1,112 @@
+import os, sys, time, json, copy, glob, random, datetime
+import scipy.sparse as sp
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from collections import Counter, defaultdict
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn import Parameter as Param
+
+from torch_geometric.nn.models import GAE
+from torch_geometric.nn.conv import GCNConv, MessagePassing
+from torch_geometric.data import Data
+from torch_geometric.utils import add_self_loops, degree
+
+from utils import *
+
+
+def main():
+    cur_path = os.getcwd()
+    dataset_path = os.path.join(get_parent_path(cur_path, 1), "Dataset")
+
+    ############################################################
+    ### load multimodal network dataset
+    ############################################################
+
+    print("nodes start")
+    # load nodes
+    node_names = ["drug", "disease", "protein", "mrna", "mirna", "metabolite"]
+    node_symbols = {
+        "drug": "d",
+        "disease": "z",
+        "protein": "p",
+        "mrna": "m",
+        "mirna": "mi",
+        "metabolite": "b",
+    }
+    symbol_to_nodename = {v: k for k, v in node_symbols.items()}
+    nodes = load_nodes(dataset_path, node_names)
+    node_nums = {node: count_unique_ids(nodes[node]) for node in node_names}
+
+    print("edges start")
+    # load edges
+    unweighted_edge_names = [
+        "disease_metabolite",
+        "disease_mirna",
+        "disease_mrna_down",
+        "disease_mrna_up",
+        "disease_protein",
+        "drug_disease",
+        "drug_drug",
+        "drug_mrna_down",
+        "drug_mrna_up",
+        "drug_metabolite_down",
+        "drug_metabolite_up",
+        "drug_protein",
+        "protein_protein",
+    ]
+    weighted_edge_names = [
+        "disease_disease",
+        "drug_mirna",
+        "protein_mrna",
+        "protein_mirna",
+        "mrna_mrna",
+        "mrna_mirna",
+        "mirna_mirna",
+    ]
+    edge_symbols = {
+        "disease_disease": "z_z",
+        "disease_metabolite": "z_b",
+        "disease_mirna": "z_mi",
+        "disease_mrna_down": "z_m_down",
+        "disease_mrna_up": "z_m_up",
+        "disease_protein": "z_p",
+        "drug_disease": "d_z",
+        "drug_drug": "d_d",
+        "drug_mrna_down": "d_m_down",
+        "drug_mrna_up": "d_m_up",
+        "drug_metabolite_down": "d_b_down",
+        "drug_metabolite_up": "d_b_up",
+        "drug_protein": "d_p",
+        "drug_mirna": "d_mi",
+        "mirna_mirna": "mi_mi",
+        "mrna_mirna": "m_mi",
+        "mrna_mrna": "m_m",
+        "protein_protein": "p_p",
+        "protein_mirna": "p_mi",
+        "protein_mrna": "p_m",
+    }
+    symbol_to_edgename = {v: k for k, v in edge_symbols.items()}
+    edges, edge_weights = load_edges(
+        dataset_path, unweighted_edge_names, weighted_edge_names
+    )
+
+    print("embs start")
+    # load attributes
+    att_names = ["drug_ecfp", "protein_seq"]
+    att_symbols = {"drug": "drug_ecfp", "protein": "protein_seq"}
+    atts = load_attributes(
+        nodes, att_names, node_nums, MXLEN=nodes["protein"].SeqLength.max()
+    )
+    print("fin")
+
+
+if __name__ == "__main__":
+    main()
--- src_py/utils.py
+++ src_py/utils.py
@@ -1,1 +1,111 @@
+import os, sys, glob
+import scipy.sparse as sp
 import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from torch import Tensor
+
+
+def get_parent_path(pth, depth=1):
+    for _ in range(depth):
+        pth = os.path.dirname(pth)
+    return pth
+
+
+def glob_files(pth):
+    ret = [f for f in glob.glob(os.path.join(pth, "*"))]
+    return ret
+
+
+def get_pair(arr):
+    return set([(l[0], l[1]) for l in arr.T.tolist()])
+
+
+def union_set(s1, s2):
+    return s1.union(s2)
+
+
+def measure_time(txt, time_begin, time_fin):
+    time = time_fin - time_begin
+    print("{}: {:0.3f}".format(txt, time))
+
+
+def count_unique_ids(var):
+    if isinstance(var, pd.DataFrame):
+        ret = len(var["MyID"].unique())
+        return ret
+    else:
+        try:
+            ret = len(set([x["id"] for x in var.values()]))
+            return ret
+        except:
+            raise Exception("Dtype error: Node file")
+
+
+def load_nodes(path, keys):
+    nodes = dict()
+    for key in keys:
+        filepath = os.path.join(path, "Node", "{}.csv".format(key))
+        assert os.path.exists(filepath)
+        nodes[key] = pd.read_csv(filepath)
+    return nodes
+
+
+def load_edges(path, unweighted_keys, weighted_keys):
+    edge_indexes = dict()
+    edge_weights = dict()
+
+    # load unweighted network
+    for key in unweighted_keys:
+        filepath = os.path.join(path, "Edge", "{}.npy".format(key))
+        assert os.path.exists(filepath)
+        edge = sp.coo_matrix(np.load(filepath))
+        edge = Tensor(np.array([edge.row, edge.col])).long()
+        edge_indexes[key] = edge
+
+    # load weighted network
+    for key in weighted_keys:
+        filepath = os.path.join(path, "Edge", "{}.npy".format(key))
+        assert os.path.exists(filepath)
+        edge = np.load(filepath)
+        row, col = edge.nonzero()
+        edge_indexes[key] = Tensor([row, col]).long()
+        edge_weights[key] = Tensor(np.array([edge[i][j] for i, j in zip(row, col)]))
+
+    return edge_indexes, edge_weights
+
+
+def load_attributes(nodes, keys, nums, MXLEN=1000):
+    atts = dict()
+    drug_dim = 2048
+    prot_dim = MXLEN
+
+    # drug
+    if "drug_ecfp" in keys:
+        mat = np.zeros((nums["drug"], drug_dim))
+        mat = np.zeros((nums["drug"], drug_dim))
+        for myid in tqdm(range(nums["drug"])):
+            sm = nodes["drug"][nodes["drug"].MyID == myid]
+            vec = sm["ECFP"].iloc[0][1:-1].replace(",", "").split()
+            for x in vec:
+                mat[myid][int(x)] = 1
+        atts["drug_ecfp"] = mat
+
+    # protein
+    if "protein_seq" in keys:
+        seqs = nodes["protein"]["Sequence"].values
+        uniq_chars = set()
+        char_to_id = {}
+        for seq in seqs:
+            uniq_chars = uniq_chars.union(set(list(seq)))
+        for e, x in enumerate(list(uniq_chars)):
+            char_to_id[x] = e
+        mat = np.empty((len(seq), len(char_to_id), prot_dim))
+        for i, s in enumerate(tqdm(seq)):
+            if len(s) > MXLEN:
+                s = s[:MXLEN]
+            for j, c in enumerate(s):
+                mat[i][char_to_id[c]][j] = 1
+        atts["protein_seq"] = mat
+
+    return atts
