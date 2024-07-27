0e4ec258e79f267d2faa06054df36cc1ca9a12e9
Yuto Ohnuki <yuuto.personal92@gmail.com>
2024-07-28 05:13:43 +0900

    update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -53,3 +53,54 @@ def show_max_values(auroc, auprc, acc):
     print("AUROC: {}".format(max(auroc)))
     print("AUPRC: {}".format(max(auprc)))
     print("ACC:   {}".format(max(acc)))
+
+
+def show_results(train_record, valid_record, best_record, conf):
+    line = "#" * 40
+    print(line)
+    print("Results:")
+    
+    ret_indexes = []
+    ret_train_aurocs, ret_train_auprcs, ret_train_accs = [], [], []
+    ret_valid_aurocs, ret_valid_auprcs, ret_valid_accs = [], [], []
+    
+    # Training/Validation records
+    for cv in range(conf.cv):
+        cv_train_record = np.array(train_record[cv]).T
+        cv_valid_record = np.array(valid_record[cv]).T
+        best_index = np.argmax(cv_valid_record[1])
+        ret_indexes.append(best_index)
+        
+        ret_train_aurocs.append(cv_train_record[0][best_index])
+        ret_train_auprcs.append(cv_train_record[1][best_index])
+        ret_train_accs.append(cv_train_record[2][best_index])
+        ret_valid_aurocs.append(cv_valid_record[0][best_index])
+        ret_valid_auprcs.append(cv_valid_record[1][best_index])
+        ret_valid_accs.append(cv_valid_record[2][best_index])
+    
+    # Output as dataframe
+    ret = {
+        "Train: AUROC": np.array(ret_train_aurocs),
+        "Train: AUPRC": np.array(ret_train_auprcs),
+        "Train: ACC": np.array(ret_train_accs),
+        "Valid: AUROC": np.array(ret_valid_aurocs),
+        "Valid: AUPRC": np.array(ret_valid_auprcs),
+        "Valid: ACC": np.array(ret_valid_accs)
+    }
+    
+    ret = pd.DataFrame(ret)
+    ret.index.name = "CV"
+    display(ret)
+    
+    # Test performance
+    best_cv = np.argmax(ret_valid_auprcs)
+    result = best_records[best_cv][-1]
+    
+    print("\tTest:")
+    perf = {
+        "Test: AUROC": np.array(result["AUROC"]),
+        "Test: AUPRC": np.array(result['AUPRC']),
+        "Test: ACC": np.array(result["ACC"])
+    }
+    perf = pd.DataFrame(perf, index=[0])
+    display(perf)
